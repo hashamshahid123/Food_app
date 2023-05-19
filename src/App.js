c3bd52453948b8d6d43ca50bde1ffd7ab8a96327
hashamshahid123 <hashamshahid071@gmail.com>
2023-05-19 15:43:02 -0700

    sample commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Food from './components/Food';
+import HeadlineCard from './components/HeadlineCard';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+     <Navbar/>
+     <Hero/>
+     <HeadlineCard/>
+     <Food/>
+    </div>
+  );
+}
+
+export default App;
